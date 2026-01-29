3c53b449faabe282a5f0ecbc5d058dbe6902f33d
0xward <aradeawardana97@gmail.com>
2026-01-29 07:58:46 +0700

    Create hardhat.config.js

---
+++ contracts/hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    base: {
+      url: "https://mainnet.base.org",
+      accounts: ["<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>"]
+    }
+  }
+};
